59c8732eede6a5b518caf06a3c3d680c083f405e
John Schiltz <johnschiltz2000@gmail.com>
2022-08-23 17:38:56 -0500

    rename major to major_code

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ function saveData(filename: string, data: any) {
 
 // getData();
 const semester = "Fall"
-const major = "COMPSCI"
+const major_code = "COMPSCI"
 // get_classes(year, major)
 const driver_path = 'E:/git/CSSC-Class-Generator/chromedriver_win32_104/chromedriver.exe'
 const driver = setup(driver_path)
 
-get_classes(driver, semester, major).then((classes) => {
-    saveData(major + "-" + semester + "-Class-List", classes)
+get_classes(driver, semester, major_code).then((classes) => {
+    saveData(major_code + "-" + semester + "-Class-List", classes)
 })
